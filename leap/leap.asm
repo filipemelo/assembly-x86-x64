376af8134ea04d4a11f880d6c0bcb52ce0947d92
filipemelo <filipemelo.br@gmail.com>
2021-10-13 03:11:44 -0300

    Improving division by 100

--- leap/leap.asm
+++ leap/leap.asm
@@ -1,20 +1,18 @@
 section .text
 global leap_year
 
-%macro divide_by 1
-    mov rax, rdi
-    mov rbx, %1
-    xor rdx, rdx
-    div rbx ;idiv rbx
-    test rdx, rdx ;cmp rdx, 0
+%macro divide_by_100 0 
+    mov eax, 1	
+	imul ecx, edi, -1030792151	; multiply by modular inverse of 25
+	cmp	ecx, 171798688			; compare to floor((2**32-1)/25)
 %endmacro
 
 leap_year:
     test dil, 3 ; test rdi, 3 0b0011
     jne false
 
-    divide_by 100
-    jne true
+    divide_by_100 ; facilitate the code reading
+    ja true ; if ~(25|n), return 1
 
     test edi, 15; 0b1111
     jne false
